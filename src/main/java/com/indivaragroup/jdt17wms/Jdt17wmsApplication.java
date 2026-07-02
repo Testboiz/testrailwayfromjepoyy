0ddfe232a8a4ed7226c9d6c35fa13fe273f6fa27
Manuel <manuelzefanyaaa@gmail.com>
2026-07-02 21:07:22 +0700

    first init

---
+++ src/main/java/com/indivaragroup/jdt17wms/Jdt17wmsApplication.java
@@ -0,0 +1,13 @@
+package com.indivaragroup.jdt17wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdt17wmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdt17wmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indivaragroup/jdt17wms/Jdt17wmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indivaragroup.jdt17wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdt17wmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
