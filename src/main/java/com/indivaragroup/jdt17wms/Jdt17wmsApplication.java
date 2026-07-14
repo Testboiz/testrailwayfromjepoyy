21119bafcad48b28da0b73176000698bcef78fd0
Testboiz <rioferdian2004@gmail.com>
2026-07-14 11:30:16 +0700

    refactor : refactor the Clock into separate config file

--- src/main/java/com/indivaragroup/jdt17wms/Jdt17wmsApplication.java
+++ src/main/java/com/indivaragroup/jdt17wms/Jdt17wmsApplication.java
@@ -2,17 +2,11 @@ package com.indivaragroup.jdt17wms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Jdt17wmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jdt17wmsApplication.class, args);
-	}
-
-	@Bean
-	public java.time.Clock clock() {
-		return java.time.Clock.systemUTC();
 	}
 }
