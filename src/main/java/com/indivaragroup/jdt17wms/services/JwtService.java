d87176733390dbcb40eb387a0f0b1fd4b5145e75
Manuel <manuelzefanyaaa@gmail.com>
2026-07-17 01:27:16 +0700

    fix: remove unused hardcoded JWT default secret from compiled binary

--- src/main/java/com/indivaragroup/jdt17wms/services/JwtService.java
+++ src/main/java/com/indivaragroup/jdt17wms/services/JwtService.java
@@ -39,8 +39,6 @@ public class JwtService {
     private static final String USER_ROLE_CLAIM = "role";
     private static final String USER_NAME_CLAIM = "name";
 
-    // Minimum 256-bit key for HS256
-    private static final String DEFAULT_SECRET = "indivaragroupwmsjsonwebtokensecretkey2026supersecretkey";
 
     @Value("${jwt.secret}")
     private String secretKey;
